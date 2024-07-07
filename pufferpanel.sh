14baf9daef098fb635d4ae7fa68bcf1e0d230a45
Butter2605 <121375931+Butter2605@users.noreply.github.com>
2024-07-07 22:01:33 +0700

    Update pufferpanel.sh

--- pufferpanel.sh
+++ pufferpanel.sh
@@ -1,9 +1,12 @@
 # A Script to auto install Pufferpanel 
 
+# First to install everything this machine needs
+apt install curl sudo neofetch systemctl -y
+
 curl -s https://packagecloud.io/install/repositories/pufferpanel/pufferpanel/script.deb.sh | sudo bash
 sudo apt-get install pufferpanel
 sudo systemctl enable pufferpanel
-sudo pufferpanel user add --email foxytoux@gmail.com --name foxytoux --password Fox21200 --admin
+sudo pufferpanel user add --email tungnguyen7838@gmail.com --name admin --password admin123 --admin
 sudo systemctl enable --now pufferpanel
 
 # I uses https://docs.pufferpanel.com/en/2.x/installing.html to make this easy script, please look over there is there is any problem
